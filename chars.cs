4099f3d529e7307968441e24cd04eda3714aba0f
Atilla Burak Artıran <83754548+Achirura1048@users.noreply.github.com>
2024-05-05 12:40:57 +0300

    Add files via upload

---
+++ chars.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAN_PİSU_ISREAL
+{
+     internal class chars
+    {
+        public static one_piece luffy = new one_piece
+        {
+            id = 1,
+            name = "Luffy",
+            gender = "Male",
+            crew = "Strawhat Pirates",
+            df = "Zoan",
+            conq_haki = "Ok",
+            arm_haki = "Ok",
+            obv_haki = "Ok",
+            bounty = 3000000000,
+            origin = "East Blue",
+            arc = "Romance Down"
+        };
+
+        public static one_piece zoro = new one_piece 
+        { 
+            id = 2,
+            name = "Zoro",
+            gender = "Male",
+            crew = "Strawhat Pirates",
+            df = "None",
+            conq_haki = "Ok",
+            arm_haki = "Ok",
+            obv_haki = "Ok",
+            bounty = 1111000000,
+            origin = "East Blue",
+            arc = "Romance Down",
+};
+        public static one_piece nami = new one_piece
+        {
+            id = 3,
+            name = "Nami",
+            gender = "Female",
+            crew = "Strawhat Pirates",
+            df = "None",
+            conq_haki = "None",
+            arm_haki = "None",
+            obv_haki = "None",
+            bounty = 366000000,
+            origin = "East Blue",
+            arc = "Orange Town"
+        };
+
+        public static one_piece usopp = new one_piece
+        {
+            id = 4,
+            name = "Usopp",
+            gender = "Male",
+            crew = "Strawhat Pirates",
+            df = "None",
+            conq_haki = "None",
+            arm_haki = "None",
+            obv_haki = "Ok",
+            bounty = 500000000,
+            origin = "East Blue",
+            arc = "Syrup Village"
+        };
+
+        public static one_piece sanji = new one_piece
+        {
+            id = 5,
+            name = "Sanji",
+            gender = "Male",
+            crew = "Strawhat Pirates",
+            df = "None",
+            conq_haki = "None",
+            arm_haki = "Ok",
+            obv_haki = "Ok",
+            bounty = 1032000000,
+            origin = "North Blue",
+            arc = "Baratie"
+        };
+
+        public static one_piece chopper = new one_piece
+        {
+            id = 6,
+            name = "Chopper",
+            gender = "Male",
+            crew = "Strawhat Pirates",
+            df = "Zoan",
+            conq_haki = "None",
+            arm_haki = "None",
+            obv_haki = "None",
+            bounty = 1000,
+            origin = "Grand Line",
+            arc = "Drum Island"
+        };
+
+        public static one_piece robin = new one_piece
+        {
+            id = 7,
+            name = "Robin",
+            gender = "Female",
+            crew = "Strawhat Pirates",
+            df = "Paramecia",
+            conq_haki = "None",
+            arm_haki = "None",
+            obv_haki = "None",
+            bounty = 930000000,
+            origin = "West Blue",
+            arc = "Whiskey Peak"
+        };
+
+        public static one_piece franky = new one_piece
+        {
+            id = 8,
+            name = "Franky",
+            gender = "Male",
+            crew = "Strawhat Pirates",
+            df = "None",
+            conq_haki = "None",
+            arm_haki = "None",
+            obv_haki = "None",
+            bounty = 394000000,
+            origin = "Grand Line",
+            arc = "Water 7"
+        };
+
+        public static one_piece brook = new one_piece
+        {
+            id = 9,
+            name = "Brook",
+            gender = "Male",
+            crew = "Strawhat Pirates",
+            df = "Paramecia",
+            conq_haki = "None",
+            arm_haki = "None",
+            obv_haki = "None",
+            bounty = 383000000,
+            origin = "West Blue",
+            arc = "Thriller Bark"
+        };
+
+        public static one_piece jimbe = new one_piece
+        {
+            id = 10,
+            name = "Jimbe",
+            gender = "Male",
+            crew = "Strawhat Pirates",
+            df = "None",
+            conq_haki = "None",
+            arm_haki = "Ok",
+            obv_haki = "Ok",
+            bounty = 1100000000,
+            origin = "Grand Line",
+            arc = "Impel Down"
+        };
+
+        public static one_piece coby = new one_piece
+        {
+            id = 11,
+            name = "Coby",
+            gender = "Male",
+            crew = "Marines",
+            df = "None",
+            conq_haki = "None",
+            arm_haki = "Ok",
+            obv_haki = "Ok",
+            bounty = 500000000,
+            origin = "East Blue",
+            arc = "Romance Dawn"
+        };
+
+        public static one_piece shanks = new one_piece
+        {
+            id = 12,
+            name = "Shanks",
+            gender = "Male",
+            crew = "Red Hair Pirates",
+            df = "None",
+            conq_haki = "Ok",
+            arm_haki = "Ok",
+            obv_haki = "Ok",
+            bounty = 4048900000,
+            origin = "West Blue",
+            arc = "Romance Dawn"
+        };
+
+        public static one_piece ace = new one_piece
+        {
+            id = 13,
+            name = "Ace",
+            gender = "Male",
+            crew = "Whitebeard Pirates",
+            df = "Logia",
+            conq_haki = "Ok",
+            arm_haki = "Ok",
+            obv_haki = "Ok",
+            bounty = 550000000,
+            origin = "South Blue",
+            arc = "Drum Island"
+        };
+
+        public static one_piece sabo = new one_piece
+        {
+            id = 14,
+            name = "Sabo",
+            gender = "Male",
+            crew = "Revolutionary Army",
+            df = "Logia",
+            conq_haki = "None",
+            arm_haki = "Ok",
+            obv_haki = "Ok",
+            bounty = 602000000,
+            origin = "East Blue",
+            arc = "Post-war"
+        };
+
+
+        public static one_piece bb = new one_piece
+        {
+            id = 14,
+            name = "Black Beard",
+            gender = "Male",
+            crew = "Black Beard Pirates",
+            df = "Logia",
+            df2 = "Paramecia",
+            conq_haki = "None",
+            arm_haki = "Ok",
+            obv_haki = "Ok",
+            bounty = 602000000,
+            origin = "Grand Line",
+            arc = "Skypiea"
+        };
+
+        public static one_piece[] karakterler = new one_piece[]
+ {
+                luffy, zoro, nami, usopp ,
+                sanji , chopper , robin , franky ,
+                brook , jimbe , coby ,shanks ,
+                ace , sabo , bb
+
+ };
+
+    }
+}
---
+++ one_piece.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAN_PİSU_ISREAL
+{
+    public  class one_piece
+    {
+        public int id;
+        public string name;
+        public string gender;
+        public string crew;
+        public string df;
+        public string df2;
+        public string conq_haki;
+        public string arm_haki;
+        public string obv_haki;
+        public uint bounty;
+        public string origin;
+        public string arc;
+
+        
+
+
+
+    }
+
+
+}
+
